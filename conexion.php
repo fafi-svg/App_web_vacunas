0308585e8ede20acb6f7d450bf5492c7ae492d60
fafi-svg <rfaelsolanob@gmail.com>
2023-10-10 20:35:11 -0500

    save attribute = database

--- conexion.php
+++ conexion.php
@@ -1,26 +1,24 @@
 <?php
-    class conexion{
+    class ConexionDataBase{
         public $SERVER;
         public $PORT;
         public $USER;
         public $PASS;
         public $DATABASE;
         public $MYSQLI;
-        public static function conexiondb(){
+        public static function conexion(){
             try{
-                $mysqli = new mysqli(
-                    $_ENV['SERVER'],
-                    $_ENV['USER'],
-                    $_ENV['PASS'],
-                    $_ENV['DATABASE'],
-                    $_ENV['PORT']
-                );
+                $SERVER = $_ENV['SERVER'];
+                $USER = $_ENV['USER'];
+                $PASS = $_ENV['PASS'];
+                $DATABASE = $_ENV['DATABASE'];
+                $PORT = $_ENV['PORT'];
+                $mysqli = new mysqli($_ENV['SERVER'],$_ENV['USER'],$_ENV['PASS'],$_ENV['DATABASE'],$_ENV['PORT']);
                 return $mysqli;
             }
             catch(exeption $e){
-                echo $e;
+                return $e;
             }
-            
         }
     }
 
