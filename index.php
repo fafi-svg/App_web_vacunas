6524304716fe96c80ca67befafd9e3df5e400c92
fafi-svg <rfaelsolanob@gmail.com>
2023-10-25 03:44:24 -0500

    index.php

--- index.php
+++ index.php
@@ -7,18 +7,20 @@
 </head>
 <body>
     <?php
-        require_once __DIR__."/vendor/autoload.php";
-        use Dotenv\Dotenv;
-        $dotenv = Dotenv::createImmutable(__DIR__);
-        $dotenv->load();
         require_once __DIR__ . "/conexion.php";
-        $mysqli = ($Conexion = (new ConexionDataBase)->conexion());
-        if(!($mysqli->connect_errno)){
-           echo "Conexion Correcta";
-        }
-        else{
-            echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-        }
+        $Conexion = ((new ConexionDataBase)->conexion());
+        if(!($Conexion->connect_errno)){
+            // echo "Conexion Correcta";
+            // echo $_ENV['SERVER']; 
+            header("location: login");
+            die();
+         }
+         else{
+             echo "Fallo al conectar a MySQL: (" . $Conexion->connect_errno . ") " . $Conexion->connect_error;
+         }
+    ?>
+    <?php
+
     ?>
 </body>
 </html>
