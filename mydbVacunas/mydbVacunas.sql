a0ec64d0176ad4e67a9764fef1b7dabd4d1dee4d
fafi-svg <rfaelsolanob@gmail.com>
2023-11-10 12:42:06 -0500

    Update DataBase Insert [Mascotas-Razas]

--- mydbVacunas/mydbVacunas.sql
+++ mydbVacunas/mydbVacunas.sql
@@ -102,7 +102,7 @@ DEFAULT CHARACTER SET = utf8mb3;
 -- Table `mydbvacunas`.`mascota`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydbvacunas`.`mascota` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL auto_increment,
   `nombre` VARCHAR(150) NULL DEFAULT NULL,
   `FechaNacimiento` DATETIME NULL DEFAULT NULL,
   `foto` BLOB NULL DEFAULT NULL,
@@ -174,13 +174,43 @@ insert into Role values('2','admin'), ('1','user');
 insert into User values('2','fafi','user','examle2@correo.com','$2y$10$ekpW/Y2KnXXMwaVUXveU3.HyV1m.VhT/KD6mDgYzKX2Pw1OAEZ4MG','1','null'),('1','rafael','admin','examle3@correo.com','$2y$10$2GKBB5QXkAypiof4drhwpOJ.zur4njTdpq01Iq0Soec7dP7A5MAQW','2','null'),('3', 'Ricardo', 'UseR-1', 'example4@correo.com', '$2y$10$INUrk34z0.OzzwAu25XvceLGwXw0q3ORKH4WBss9kXfF.W2O8rDpm', '1', 'null'),('4', 'Ricardo', 'admin-2', 'example5@correo.com', '$2y$10$INUrk34z0.OzzwAu25XvceLGwXw0q3ORKH4WBss9kXfF.W2O8rDpm', '2', 'null');
 insert into TipoMascota (id,nombre, EdadEquivalenteInfante, EdadEquivalenteJoven, EdadEquivalenteAdolecente, EdadAdulto) value('1','gato','2','5','8','10'), ('2','perro','3','6','12','18');
 insert into TamanosMascota value('1','mini'),('2','pequeno'),('3','mediano'),('4','grande'),('5','gigante');
-insert into raza (nombre, TipoMascota_id, TamanoMascota_id) values('Papillón','2','1'),('Spitz Japonés Mini','2','1');
-insert into raza (nombre, TipoMascota_id, TamanoMascota_id) values('Chihuahua','2','2'),('Jack Russel Terrier','2','2'),('Fox Terrier','2','2'),('Pomerania','2','2'),('Bichón maltés','2','2'),('Beagle','2','2'),('Caniche','2','2'), ('Bulldog Francés','2','2'),('Corgi','2','2'),('Schnauzer','2','2'),('Shih tzu','2','2'), ('Jack Russell Terrier','2','2');
-insert into raza (nombre, TipoMascota_id, TamanoMascota_id) values('Border Collie','2','3'),('Boxer','2','3'),('Cocker Spaniel Inglés','2','3'),('Setter Irlandés','2','3'),('Shiba Inu','2','3'),('Labradoodle','2','3'),('Shar pei','2','3'),('Podenco andaluz','2','3'), ('Bull Terrier','2','3'),('Podenco','2','3'), ('Spaniel bretón','2','3');
-insert into raza (nombre, TipoMascota_id, TamanoMascota_id) values('Braco de Weimar','2','4'),('Pastor alemán','2','4'),('Setter inglés','2','4'),('Bullmastiff','2','4'),('San Bernardo','2','4'),('Gran Danés','2','4'),('Mastín Inglés','2','4'),('Bobtail','2','4'),('Dálmata','2','4'),('Mastín italiano','2','4'),('Dogo argentino','2','4'),('Pitbull','2','4'),('Rottweiler','2','4'),('Akita Inu','2','4') ,('Mastín del Pirineo','2','4'),('Boyero de Berna','2','4'),('Samoyedo','2','4'),('Mastín español','2','4') ,('Malamute de Alaska','2','4'),('Boyero de Berna','2','4'),('Galgo','2','4'),('Labrador','2','4') ,('Bloodhound','2','4'),('Boyero de Berna','2','4');
-insert into Vacunas (id,nombre, tipomascota_id, Aplicacion) value('1','Parvovirosis','2','45'),('2','Pentavalente ','2','60'),('3','Pentavalente + coronavirus','2','75'),('4','Pentavalente + coronavirus','2','90 '),('5','Rabia ','2','120'),('6','Tos de perreras','2','0'),('7','Polivalente ','2','180'),('8','Triple felina','1','90'),('9','Triple felina','1','195'),('10','Rabia','1','135 '),('11','Leucemia ','1','150'),('12','Triple felina','1','365'),('13','Leucemia ','1','0'),('14','Rabia ','1','365');
-select * from Vacunas ;
-select * from user;
+-- -----------------------------------------------------------------------------##
+-- Razas Perros
+-- -----------------------------------------------------------------------------##
+insert into raza (id, nombre, TipoMascota_id, TamanoMascota_id) values ('1','Papillón','2','1'),('2','Spitz Japonés Mini','2','1');
+insert into raza (id, nombre, TipoMascota_id, TamanoMascota_id) values ('3','Chihuahua','2','2'),('4','Jack Russel Terrier','2','2'),('5','Fox Terrier','2','2'),('6','Pomerania','2','2'),('7','Bichón maltés','2','2'),('8','Beagle','2','2'),('9','Caniche','2','2'), ('10','Bulldog Francés','2','2'),('11','Corgi','2','2'),('12','Schnauzer','2','2');
+insert into raza (id, nombre, TipoMascota_id, TamanoMascota_id) values ('13','Shih tzu','2','2'), ('14','Jack Russell Terrier','2','2');
+insert into raza (id, nombre, TipoMascota_id, TamanoMascota_id) values ('15','Border Collie','2','3'),('16','Boxer','2','3'),('17','Cocker Spaniel Inglés','2','3'),('18','Setter Irlandés','2','3'),('19','Shiba Inu','2','3'),('20','Labradoodle','2','3'),('21','Shar pei','2','3'),('22','Podenco andaluz','2','3'), ('23','Bull Terrier','2','3');
+insert into raza (id, nombre, TipoMascota_id, TamanoMascota_id) values ('24','Podenco','2','3'), ('25','Spaniel bretón','2','3');
+insert into raza (id, nombre, TipoMascota_id, TamanoMascota_id) values ('26','Braco de Weimar','2','4'),('27','Pastor alemán','2','4'),('28','Setter inglés','2','4'),('29','Bullmastiff','2','4'),('30','San Bernardo','2','4'),('31','Gran Danés','2','4'),('32','Mastín Inglés','2','4'),('33','Bobtail','2','4'),('34','Dálmata','2','4');
+insert into raza (id, nombre, TipoMascota_id, TamanoMascota_id) values ('35','Mastín italiano','2','4'),('36','Dogo argentino','2','4'),('37','Pitbull','2','4'),('38','Rottweiler','2','4'),('39','Akita Inu','2','4') ,('40','Mastín del Pirineo','2','4'),('41','Boyero de Berna','2','4'),('42','Samoyedo','2','4'),('43','Mastín español','2','4');
+insert into raza (id, nombre, TipoMascota_id, TamanoMascota_id) values ('44','Malamute de Alaska','2','4'),('45','Boyero de Berna','2','4'),('46','Galgo','2','4'),('47','Labrador','2','4') ,('48','Bloodhound','2','4'),('49','Boyero de Berna','2','4');
+insert into Vacunas (id,nombre, tipomascota_id, Aplicacion) values ('1','Parvovirosis','2','45'),('2','Pentavalente ','2','60'),('3','Pentavalente + coronavirus','2','75'),('4','Pentavalente + coronavirus','2','90 '),('5','Rabia ','2','120'),('6','Tos de perreras','2','0');
+insert into Vacunas (id,nombre, tipomascota_id, Aplicacion) values ('7','Polivalente ','2','180'),('8','Triple felina','1','90'),('9','Triple felina','1','195'),('10','Rabia','1','135 '),('11','Leucemia ','1','150'),('12','Triple felina','1','365'),('13','Leucemia ','1','0'),('14','Rabia ','1','365');
+-- -----------------------------------------------------------------------------##
+-- Razas Gatos
+-- -----------------------------------------------------------------------------##
+insert into raza (nombre, TipoMascota_id, TamanoMascota_id) values ('Abisinio','1','3'),('Americano de pelo duro','1','4'),('Asiático','1','4'),('Azul ruso','1','4'),('Balinés','1','3'),('Bengalí','1','4'),('Birmano','1','3'),('Bobtail japonés de pelo corto','1','2');
+insert into raza (nombre, TipoMascota_id, TamanoMascota_id) values ('Bobtail japonés de pelo largo','1','3'),('Bombay','1','3'),('Bosque de Noruega','1','3'),('Bosque de Siberia','1','4'),('Británico de pelo corto','1','4'),('Burmés','1','3'),('Burmilla','1','3'),('Chinchilla','1','3');
+insert into raza (nombre, TipoMascota_id, TamanoMascota_id) values ('Cornish rex','1','3'),('Cymric','1','4'),('Devon Rex','1','2'),('Exótico de pelo corto','1','3'),('Fold escocés','1','3'),('Khao Manee','1','3'),('Korat','1','3'),('Laperm','1','3'),('Maine coon','1','5'),('Manx','1','4');
+insert into raza (nombre, TipoMascota_id, TamanoMascota_id) values ('Mau egipcio','1','3'),('Mist australiano','1','3'),('Munchkin','1','3'),('Ocigato','1','3'),('Oriental de pelo corto','1','3'),('Oriental de pelo largo','1','3'),('Persa de pelo largo','1','3'),('Pixie bob','1','4');
+insert into raza (nombre, TipoMascota_id, TamanoMascota_id) values ('Ragdoll','1','4'),('Savannah','1','3'),('Selkirk rex','1','3'),('Siamés','1','3'),('Singapura','1','2'),('Snowshoe','1','3'),('Somalí','1','3'),('Sphynx','1','3'),('Tiffanie','1','3'),('Tonquinés','1','3'),('Van turco','1','4');
+-- https://www.purina.es/encuentra-mascota/razas-de-gato
+-- -----------------------------------------------------------------------------##
+-- Mascotas Gatos
+-- -----------------------------------------------------------------------------##
+insert into mascota (nombre, FechaNacimiento, User_id, TipoMascota_id, Raza_id) values('tula','2015-05-15','4','1','50'),('peka','2015-05-15','4','1','67'),('luis','2015-05-15','3','1','93'),('nacho','2015-05-15','3','1','55'),('pepa','2015-05-15','3','1','85'),('michulais','2015-05-15','1','1','73'); 
+-- -----------------------------------------------------------------------------##
+-- Mascotas perros
+-- -----------------------------------------------------------------------------##
+insert into mascota (nombre, FechaNacimiento, User_id, TipoMascota_id, Raza_id) values('luca','2015-05-15','4','2','5'),('donkan','2015-05-15','4','2','45'),('silvestre','2015-05-15','3','2','3'),('paco','2015-05-15','3','2','15'),('miguel','2015-05-15','3','2','89'),('firulais','2015-05-15','1','2','5'); 
+-- -----------------------------------------------------------------------------##
+-- CONSULTAS
+-- -----------------------------------------------------------------------------##
+ select * from Vacunas ;
+ select * from user;
+ select * from raza;
+ select * from mascota;
 -- select count(id) from Vacunas ;
 -- SELECT v.id as ID, v.nombre as 'Nombre Vacuna', v.aplicacion as 'Dias Aplicacion', v.tipomascota_id as 'Tipo Mascota', count(c.vacuna_id) as count FROM vacunas as v left join controlvacunas as c on c.vacuna_id = v.id group by  v.id , v.nombre,  v.aplicacion , v.tipomascota_id;
 -- select t.nombre from tipomascota as t left join vacunas as v on v.tipomascota_id = t.id group by t.nombre;
